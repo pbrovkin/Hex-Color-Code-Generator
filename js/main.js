c20f046be1f76eb74f61892b2e833218c45679fa
pbrovkin <pbrovkin@gmail.com>
2020-01-14 20:44:11 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,58 +1,61 @@
+
 let div = document.getElementById('main-container');
-document.body.append(div);
 
-let blocks = div.getElementsByClassName('block');
 
-let timeInter;
+function generateColors() {
 
-function generateColor() {
     let inputVal = document.getElementById('inputValue').value;
+
     if (valueIsCorrect(inputVal)) {
         for (let i = 1; i <= inputVal; i++) {
-            let block = document.createElement('div');
-            block.className = 'block';
-            
-            let left = document.createElement('div');
-            left.className = 'block-element text';
-            let color = randomColor();
-            left.textContent = color;
+            let colorBlock = document.createElement('div');
+            colorBlock.className = 'block';
 
-            block.style.background = color;
+            let codeDiv = document.createElement('div');
+            codeDiv.className = 'block-element text';
+            let colorCode = randomColorCode();
+            codeDiv.textContent = colorCode;
 
-            let right = document.createElement('div');
-            right.className = 'block-element';
-            
-            let button = document.createElement('button');
-            button.className = 'copy-button';
-            button.textContent = 'Copy';
-            button.addEventListener('click', function () {
-                navigator.clipboard.writeText(color);
+            colorBlock.style.background = colorCode;
+
+            let buttonDiv = document.createElement('div');
+            buttonDiv.className = 'block-element';
+
+            let copyButton = document.createElement('button');
+            copyButton.className = 'copy-button';
+            copyButton.textContent = 'Copy';
+            copyButton.addEventListener('click', function () {
+                navigator.clipboard.writeText(colorCode);
             });
 
-            block.append(left);
-            right.append(button);
-            block.append(right);
-            div.append(block);
+            colorBlock.append(codeDiv);
+            buttonDiv.append(copyButton);
+            colorBlock.append(buttonDiv);
+            div.append(colorBlock);
         }
     }
 }
 
-function randomColor() {
-    return '#' + Math.random().toString(16).slice(-6).toUpperCase();
+let colorBlocks = div.getElementsByClassName('block');
+
+let timeInter;
+
+function start() {
+    timeInter = setInterval(flashing, 1000);
 }
 
-function startTimer() {
-    timeInter = setInterval(timer, 1000);
-}
-
-function stopTimer() {
+function stop() {
     clearInterval(timeInter);
 }
 
-function timer() {
-    for (let block of blocks) {
-        block.style.background = randomColor();
+function flashing() {
+    for (let block of colorBlocks) {
+        block.style.background = randomColorCode();
     }
+}
+
+function randomColorCode() {
+    return '#' + Math.random().toString(16).slice(-6).toUpperCase();
 }
 
 function valueIsCorrect(val) {
@@ -61,6 +64,8 @@ function valueIsCorrect(val) {
     }
     return alert('Enter integer value greater than 0');
 }
+
+
 
 
 
