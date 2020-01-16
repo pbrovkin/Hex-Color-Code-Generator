84b1486fb46fa2d243f48f8befbd8cbe40a93869
pbrovkin <pbrovkin@gmail.com>
2020-01-16 09:55:04 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
+//select main-container where to generate colors
 let div = document.getElementById('main-container');
 
+//Select buttons and apply functions on them
 document.getElementById('generate').addEventListener('click', generateColors);
 
 document.getElementById('start').addEventListener('click', start);
@@ -8,10 +10,9 @@ document.getElementById('start').addEventListener('click', start);
 document.getElementById('stop').addEventListener('click', stop);
 
 
-
+//main function to generate colors
 function generateColors() {
 
-    //empty the main container, if colors are already generated
     emptyContainer(div);
 
     let inputVal = document.getElementById('inputValue').value;
@@ -46,12 +47,14 @@ function generateColors() {
     }
 }
 
+//function to empty colors, if already exists
 function emptyContainer(parent) {
     if (parent.hasChildNodes()) {
         parent.textContent = '';
     }
 }
 
+//function to check input value is correct
 function valueIsCorrect(val) {
     if (Number.isInteger(Number(val)) && val > 0) {
         return true;
@@ -59,22 +62,28 @@ function valueIsCorrect(val) {
     return alert('Enter integer value greater than 0');
 }
 
+//function to generate random color code
 function randomColorCode() {
     return '#' + Math.random().toString(16).slice(-6).toUpperCase();
 }
 
+//collect all color blocks that already generated
 let colorBlocks = div.getElementsByClassName('block');
 
+//variable to set and clear time interval
 let timeInter;
 
+//function to set time interval
 function start() {
     timeInter = setInterval(flashing, 1000);
 }
 
+//function to clear time interval
 function stop() {
     clearInterval(timeInter);
 }
 
+//function to apply time interval to each color block
 function flashing() {
     for (let block of colorBlocks) {
         block.style.background = randomColorCode();
